cb42b23d931dbd396ce8e2a9ecbad72c501a75de
Lukas Schulig <lukas.schulig@uni-greifswald.de>
2025-01-08 16:49:27 +0100

    Added ID management in InputFile

--- src/af3cli/input.py
+++ src/af3cli/input.py
@@ -1,5 +1,8 @@
 from .mixin import DictMixin
 from .ligand import Ligand
+from .bond import Bond
+from .sequence import Sequence
+from .seqid import IDRegister
 
 
 class InputFile(DictMixin):
@@ -40,6 +43,57 @@ class InputFile(DictMixin):
         self.seeds: list[int] = seeds
 
         self.ligands: list[Ligand] = []
+        self.bonded_atoms: list[Bond] = []
+        self.sequences: list[Sequence] = []
+
+        self._id_register: IDRegister = IDRegister()
+
+    def _register_ids(self) -> None:
+        """
+        Registers unique IDs for sequences and ligands in the internal ID register.
+
+        This method iterates through the objects stored in sequences and ligands,
+        checking if each entry is already registered. If an entry is not yet
+        registered, it retrieves the associated IDs by calling its `get_id`
+        method. Each ID from the returned list is then registered into the
+        internal `IDRegister`. Once the IDs are registered, the entry's
+        `is_registered` attribute is set to `True`.
+
+        Raises
+        ------
+        AttributeError
+            If an item in sequences or ligands does not have `is_registered` or
+            `get_id` attributes.
+        """
+        for seqtype in [self.sequences, self.ligands]:
+            for entry in seqtype:
+                if entry.is_registered:
+                    continue
+                seq_ids = entry.get_id()
+                if seq_ids is not None:
+                    for seq_id in seq_ids:
+                        self._id_register.register(seq_id)
+                        entry.is_registered = True
+
+
+    def _assign_ids(self) -> None:
+        """
+        Assign unique IDs to sequences and ligands that do not already have an ID.
+
+        This method iterates over sequences and ligands, checking if they have an ID.
+        If IDs are missing, it generates unique IDs for entries within each type and
+        assigns these IDs.
+        """
+        for seqtype in [self.sequences, self.ligands]:
+            for entry in seqtype:
+                if entry.get_id() is None:
+                    seq_ids = [self._id_register.generate() for _ in range(entry.num)]
+                    entry.set_id(seq_ids)
+
+
+    def _prepare(self) -> None:
+        self._register_ids()
+        self._assign_ids()
 
     def to_dict(self) -> dict:
         pass
