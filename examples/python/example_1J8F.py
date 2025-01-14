7e0b1c63fcec2669e42652e5e6a745baabc3f82c
Philipp Doepner <s-phdoep@stud.uni-greifswald.de>
2025-01-14 17:21:15 +0100

    Refactor example script for consistency and clarity

--- examples/python/example_1J8F.py
+++ examples/python/example_1J8F.py
@@ -1,5 +1,5 @@
 """
-Example 1:
+Example:
 Human SIRT2 Histone Deacetylase
     - Protein Data Bank (PDB) Entry: 1J8F
     - Reference: https://www.rcsb.org/structure/1J8F
@@ -9,25 +9,27 @@ This script provides a Python implementation as an alternative to the af3cli inp
 """
 
 import pprint
-from af3cli import JSONWriter, InputBuilder, Sequence, SequenceType, Ligand, LigandType
+from af3cli import InputBuilder, Sequence, SequenceType, Ligand, LigandType
 
 # Define constants
 FILENAME = "example_1J8F_python.py"
 JOB_NAME = "example_1J8F_py_job"
 INPUT_SEQUENCE_TYPE = SequenceType.PROTEIN
 SIRT2_SEQUENCE_STR = (
-    "GEADMDFLRNLFSQTLSLGSQKERLLDELTLEGVARYMQSERCRRVICLVGAGISTSAGIPDFRSPSTGLYDNLEKYHLPYPEAIFEISYFKKHPEPFFALA"
-    "KELYPGQFKPTICHYFMRLLKDKGLLLRCYTQNIDTLERIAGLEQEDLVEAHGTFYTSHCVSASCRHEYPLSWMKEKIFSEVTPKCEDCQSLVKPDIVFFGESLPARFFSCMQS"
-    "DFLKVDLLLVMGTSLQVQPFASLISKAPLSTPRLLINKEKAGQSDPFLGMIMGLGGGMDFDSKKAYRDVAWLGECDQGCLALAELLGWKKELEDLVRREHASIDAQS"
+    "GEADMDFLRNLFSQTLSLGSQKERLLDELTLEGVARYMQSERCRRVICLVGAGISTSAGIPDFRSPSTGLYDN"
+    "LEKYHLPYPEAIFEISYFKKHPEPFFALAKELYPGQFKPTICHYFMRLLKDKGLLLRCYTQNIDTLERIAGLE"
+    "QEDLVEAHGTFYTSHCVSASCRHEYPLSWMKEKIFSEVTPKCEDCQSLVKPDIVFFGESLPARFFSCMQSDFL"
+    "KVDLLLVMGTSLQVQPFASLISKAPLSTPRLLINKEKAGQSDPFLGMIMGLGGGMDFDSKKAYRDVAWLGECD"
+    "QGCLALAELLGWKKELEDLVRREHASIDAQS"
 )
-LIGAND_TYPE = LigandType.SMILES
-ZINC_ION_SMILE ="[Zn2+]"
+LIGAND_TYPE = LigandType.CCD
+ZINC_ION_CCD ="ZN"
 
 # Create protein sequence object
 sequence = Sequence(seq_type=INPUT_SEQUENCE_TYPE, seq_str=SIRT2_SEQUENCE_STR)
 
 # Create ligand object
-ligand = Ligand(ligand_type=LIGAND_TYPE, ligand_str=ZINC_ION_SMILE)
+ligand = Ligand(ligand_type=LIGAND_TYPE, ligand_str=ZINC_ION_CCD)
 
 # Build input configuration for the job
 input_builder = InputBuilder()
@@ -37,7 +39,7 @@ input_builder.add_ligand(ligand)
 internal_input = input_builder.build()
 
 # Uncomment following line to generate output as JSON file
-#JSONWriter.write(FILENAME, internal_input)
+#internal_input.write(FILENAME)
 
 print_json_via_debug = pprint.PrettyPrinter(indent=4)
 print_json_via_debug.pprint(internal_input.to_dict())
