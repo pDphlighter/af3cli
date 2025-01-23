a0daa4351c97f4fd344edc787924b59bac1f94c4
Lukas Schulig <schuligl@uni-greifswald.de>
2025-01-23 17:54:21 +0100

    Refactor tests to use `build()` method consistently.
    Replaced direct access to `_afinput` with `build()` calls to better align with encapsulation practices. Added test cases for `CCDLigand` and `SMILigand` and adjusted related assertions to handle new ligand types. This ensures improved test clarity and functionality.

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -4,7 +4,7 @@ import random
 from af3cli.builder import InputBuilder
 from af3cli.input import InputFile
 from af3cli.bond import Atom, Bond
-from af3cli.ligand import Ligand, LigandType
+from af3cli.ligand import Ligand, LigandType, CCDLigand, SMILigand
 from af3cli.sequence import Sequence, SequenceType
 
 
@@ -26,10 +26,23 @@ def sample_sequence() -> Sequence:
 def sample_ligand() -> Ligand:
     return Ligand(
         ligand_type=LigandType.CCD,
-        ligand_str="NAC",
+        ligand_value=["NAC"],
         seq_id=["C", "D"]
     )
 
+@pytest.fixture(scope="module")
+def sample_ccdligand() -> CCDLigand:
+    return CCDLigand(
+        ligand_value=["NAC"],
+        seq_id=["E"]
+    )
+
+@pytest.fixture(scope="module")
+def sample_smiligand() -> SMILigand:
+    return SMILigand(
+        ligand_value="CCOCC",
+        seq_id=["F"]
+    )
 
 @pytest.fixture(scope="module")
 def sample_bond() -> Bond:
@@ -40,7 +53,7 @@ def sample_bond() -> Bond:
 
 def test_builder_set_name(builder: InputBuilder) -> None:
     builder.set_name("test")
-    assert builder._afinput.name == "test"
+    assert builder.build().name == "test"
 
 
 @pytest.mark.parametrize("seeds", [
@@ -48,17 +61,17 @@ def test_builder_set_name(builder: InputBuilder) -> None:
 ])
 def test_builder_set_seeds(builder: InputBuilder, seeds: list[int]) -> None:
     builder.set_seeds(seeds)
-    assert sorted(list(builder._afinput.seeds)) == sorted(seeds)
+    assert sorted(list(builder.build().seeds)) == sorted(seeds)
 
 
 def test_builder_set_version(builder: InputBuilder) -> None:
     builder.set_version(1)
-    assert builder._afinput.version == 1
+    assert builder.build().version == 1
 
 
 def test_builder_set_dialect(builder: InputBuilder) -> None:
     builder.set_dialect("alphafold3")
-    assert builder._afinput.dialect == "alphafold3"
+    assert builder.build().dialect == "alphafold3"
 
 
 def test_builder_add_sequence(
@@ -66,32 +79,38 @@ def test_builder_add_sequence(
         sample_sequence: Sequence
 ) -> None:
     builder.add_sequence(sample_sequence)
-    assert builder._afinput.sequences[0].seq_str == sample_sequence.seq_str
-    assert builder._afinput.sequences[0].seq_type == sample_sequence.seq_type
+    input_file = builder.build()
+    assert input_file.sequences[0].seq_str == sample_sequence.seq_str
+    assert input_file.sequences[0].seq_type == sample_sequence.seq_type
 
 
 def test_builder_add_ligand(
         builder: InputBuilder,
-        sample_ligand: Ligand
+        sample_ligand: Ligand,
+        sample_ccdligand: CCDLigand,
+        sample_smiligand: SMILigand
 ) -> None:
-    builder.add_ligand(sample_ligand)
-    assert builder._afinput.ligands[0].ligand_str == sample_ligand.ligand_str
-    assert builder._afinput.ligands[0].ligand_type == sample_ligand.ligand_type
+    for i, lig in enumerate([sample_ligand, sample_ccdligand, sample_smiligand]):
+        builder.add_ligand(lig)
+        input_file = builder.build()
+        assert input_file.ligands[-1].ligand_value == lig.ligand_value
+        assert input_file.ligands[-1].ligand_type == lig.ligand_type
 
 
 def test_builder_add_bond(builder: InputBuilder, sample_bond: Bond) -> None:
     builder.add_bonded_atom_pair(sample_bond)
-    assert builder._afinput.bonded_atoms[0].atom1.eid == sample_bond.atom1.eid
-    assert builder._afinput.bonded_atoms[0].atom1.resid == sample_bond.atom1.resid
-    assert builder._afinput.bonded_atoms[0].atom1.name == sample_bond.atom1.name
-    assert builder._afinput.bonded_atoms[0].atom2.eid == sample_bond.atom2.eid
-    assert builder._afinput.bonded_atoms[0].atom2.resid == sample_bond.atom2.resid
-    assert builder._afinput.bonded_atoms[0].atom2.name == sample_bond.atom2.name
+    input_file = builder.build()
+    assert input_file.bonded_atoms[0].atom1.eid == sample_bond.atom1.eid
+    assert input_file.bonded_atoms[0].atom1.resid == sample_bond.atom1.resid
+    assert input_file.bonded_atoms[0].atom1.name == sample_bond.atom1.name
+    assert input_file.bonded_atoms[0].atom2.eid == sample_bond.atom2.eid
+    assert input_file.bonded_atoms[0].atom2.resid == sample_bond.atom2.resid
+    assert input_file.bonded_atoms[0].atom2.name == sample_bond.atom2.name
 
 
 def test_builder_user_ccd(builder: InputBuilder) -> None:
     builder.set_user_ccd("ccdString")
-    assert builder._afinput.user_ccd == "ccdString"
+    assert builder.build().user_ccd == "ccdString"
 
 
 def test_builder_build(builder: InputBuilder) -> None:
@@ -106,7 +125,7 @@ def test_builder_build(builder: InputBuilder) -> None:
 def test_builder_attach(builder: InputBuilder) -> None:
     curr_input = builder.build()
     builder.attach(curr_input)
-    assert builder._afinput == curr_input
+    assert builder.build() == curr_input
 
 
 def test_builder_reset_ids(
