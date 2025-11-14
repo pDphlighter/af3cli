e7a439d8458b6b37ae081b89f754dfc0ea0a2eed
Philipp Doepner <s-phdoep@stud.uni-greifswald.de>
2025-11-14 16:40:45 +0100

    Update the version parameter in the CLI to 4 to support the usage of optional description strings

--- src/af3cli/__main__.py
+++ src/af3cli/__main__.py
@@ -1081,7 +1081,7 @@ class CLI(CommandBase):
         self,
         filename: str = DEFAULT_FILENAME,
         jobname: str = "job",
-        version: int = 1,
+        version: int = 4,
         dialect: str = "alphafold3"
     ) -> Self:
         """
